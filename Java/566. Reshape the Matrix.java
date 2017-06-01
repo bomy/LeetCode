e4c6e63e376c3d1863943bd79f80d6a7894e4a1e
bomy <toc00337@gmail.com>
2017-06-02 01:59:46 +0800

    566. Reshape the Matrix

---
+++ Java/566. Reshape the Matrix.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+
+        if(nums.length*nums[0].length!=r*c)
+        {
+            return nums;
+        }
+
+        int[][] result=new int[r][c];
+
+        int m=0, n=0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums[0].length;j++)
+            {
+                result[m][n]=nums[i][j];
+                n++;
+
+                if(n==c)
+                {
+                    n=0;
+                    m++;
+                }
+            }
+        }
+        return result;
+    }
+}
