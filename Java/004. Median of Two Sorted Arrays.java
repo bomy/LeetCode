42b52b4a148f638a264a0eb988a12da0ad19a6ef
bomy <toc00337@gmail.com>
2017-08-04 00:57:28 +0800

    004. Median of Two Sorted Arrays.java

--- Java/004. Median of Two Sorted Arrays.java
+++ Java/004. Median of Two Sorted Arrays.java
@@ -1,24 +1,51 @@
 public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        char[] longString = s.toCharArray();
-        String  result = "" ;
-        String  temp = "";
-        int index = 0;
-
-        for(int i=0; i<longString.length; i++)
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = num1.length;
+        int n = num2.length;
+        double result  ;
+        if((m + n) % 2 == 1)
         {
-            index = temp.indexOf(longString[i]);
-
-            if( index< 0)
-                temp = temp + longString[i];
-            else
-                temp = temp.substring(index +1)+ longString[i];
-
-            if( result.length() < temp.length())
-                  result = temp ;
-            
+            result = findMidValue( num1, 0, m-1, num2, 0, n-1, (m + n) / 2);
+        }
+        else
+        {
+            result = findMidValue( num1, 0, m-1, num2, 0, n-1, (m + n - 1) / 2)
+                    + findMidValue( num1, 0, m-1, num2, 0, n-1, (m + n + 1) / 2);
         }
 
-        return result.length() ;
+
+
+        return result;
+    }
+
+    public double findMidValue(int[] num1,int mStart, int mEnd, int[] num2, int nStart, int nEnd, int k)
+    {
+        int i = ( mStart+mEnd) / 2;
+        int j = ( nStart+nEnd) / 2;
+
+        if()
+
+        if(num1[i] < num2[j])
+        {
+            if(k =<i + j )
+            {
+                 findMidValue( num1, mStart, mEnd, num2, nStart, j,  k);
+            }
+            else
+            {
+                findMidValue( num1, i, mEnd,  num2,  nStart, nEnd,  k);
+            }
+        }
+        else
+        {
+             if(k >=i + j )
+            {
+                 findMidValue( num1, mStart, mEnd,  num2,  j, nEnd,  k);
+            }
+            else
+            {
+                findMidValue( num1, mStart, i,  num2,  nStart, nEnd,  k);
+            }
+        }
     }
 }
