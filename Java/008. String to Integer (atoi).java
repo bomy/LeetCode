d56834a2587fff2d1dceb6a707db388e64454f44
bomy <toc00337@gmail.com>
2018-03-18 04:46:52 +0800

    8. String to Integer (atoi)

--- Java/008. String to Integer (atoi).java
+++ Java/008. String to Integer (atoi).java
@@ -1,28 +1,40 @@
 class Solution {
-    public int reverse(int x) {
-        String s = Integer.toString(x);
-        char[] c = s.toCharArray();
-        String temp ="";
-        for(int i = c.length-1; i>=0; i--)
-        { 
-            if( c[i] == '-')
-            {
-                temp = c[i] + temp ;  
-            }
-            else
-            {
-                temp = temp + c[i];  
-            } 
+    public int myAtoi(String str) {
+        if (str == null || str.length() == 0) 
+            return 0;  
+        
+        str = str.trim();  
+        
+        int sign = 1;
+        int j =0;
+        if(str.charAt(0) == '-') {
+            sign = -1;
+            j++;
+        } else if(str.charAt(0) == '+') {
+            j++;
         }
         
-        try
+        double result = 0;
+        for(int i = j; i<=str.length()-1;i++ )
         {
-            int ans = temp.length() >0 ? Integer.parseInt(temp) : 0;  
-            return ans;
-        } 
-        catch (Exception ex)
-        {
-            return 0;
+            int digit = str.charAt(i) - '0';
+            if(digit<0 || digit>9)
+                 break;  
+            
+            result = result*10+ digit;
         }
+        
+        result = result * sign;
+        if(Integer.MAX_VALUE < result  )
+        {
+            result = Integer.MAX_VALUE;
+        }
+        else if(  Integer.MIN_VALUE > result)
+        {
+            result = Integer.MIN_VALUE;
+        }
+            
+        int ans = (int)result;
+        return ans;
     }
 }
