2cc24ba1ad9b05164719516bf1b3e89dcb1fb3dd
bomy <toc00337@gmail.com>
2018-04-02 02:01:39 +0800

    237. Delete Node in a Linked List

---
+++ csharp/237. Delete Node in a Linked List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+		public void DeleteNode(ListNode node) {
+			 node.val = node.next.val;
+			 node.next = node.next.next;
+		}
+	}
+}
+
+
