07cf787d7f02f904cfab3aecddc0187af1808705
bomy <toc00337@gmail.com>
2019-02-24 18:40:11 +0800

    Jump Game

--- Java/045. Jump Game II.java
+++ Java/045. Jump Game II.java
@@ -2,6 +2,33 @@ class Solution {
     public int jump(int[] nums) {
         if(nums == null || nums.length <2)
 			return 0;
+		int level = 0;
+		int curMaxArea = 0;
+		int maxNext = 0;
+		int i =0;
+		
+		while(curMaxArea - i+1 <0)
+		{
+			level++;
+			for(; i< curMaxArea ;i++)
+			{
+				maxNext = Math.max(maxNext, i + nums[i]);
+				if(maxNext > nums.length-1)
+				{
+					return level;
+				}			
+			}			
+		}
+		return 0;
+		
+	
+		
+		return res;
+    }
+	
+	public int jump2(int[] nums) {
+        if(nums == null || nums.length <2)
+			return 0;
 		int res = 0;
 		int curMaxArea = 0;
 		int maxNext = 0;
